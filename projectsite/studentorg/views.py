9999140a8e03d419c94294bc9af30d8661e42b7d
AceofSpades0402 <acemanter12@gmail.com>
2025-04-05 23:30:09 +0800

    the delete and edit

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from studentorg.models import Organization
 from studentorg.forms import OrganizationForm
-from django.urls import reverse_lazy
 
 class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
-
 
 class OrganizationList(ListView):
     model = Organization
@@ -22,4 +20,15 @@ class OrganizationCreateView(CreateView):
     model = Organization
     form_class = OrganizationForm
     template_name = 'org add.html'
+    success_url = reverse_lazy('organization-list')
+
+class OrganizationUpdateView(UpdateView):
+    model = Organization
+    form_class = OrganizationForm
+    template_name = 'org_edit.html'
+    success_url = reverse_lazy('organization-list')
+
+class OrganizationDeleteView(DeleteView):
+    model = Organization
+    template_name = 'org_del.html'
     success_url = reverse_lazy('organization-list')
